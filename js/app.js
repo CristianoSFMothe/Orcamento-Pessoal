d3dcaecf7bdff1d0055ada56a2c29c5046e02c7c
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-05-02 10:48:59 -0300

    Transformando os itens em um objeto instanciavel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+class Expense {
+    constructor(year, month, day, type, description, value) {
+        this.year = year
+        this.month = month
+        this.day = day
+        this.type = type
+        this.description = description
+        this.value = value
+    }
+}
+
 function registerExpense() {
     
     // Recuperando os valores do campo
@@ -8,6 +19,17 @@ function registerExpense() {
     let description = document.getElementById('description')
     let value = document.getElementById('value')
 
-    console.log(year.value, month.value, day.value, type.value, description.value, value.value)
+    
+
+    let expense = new Expense(
+        year.value, 
+        month.value, 
+        day.value, 
+        type.value, 
+        description.value, 
+        value.value
+    )
+
+    console.log(expense)
 
 }
