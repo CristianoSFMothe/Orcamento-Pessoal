c7e446e26c8b3c20b2629df3662cf36251da4655
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-05-04 16:54:15 -0300

    Listando a consulta

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ class Expense {
 	}
 
 	validadeDate() {
-		for(let i in this) {
-			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+		for (let i in this) {
+			if (this[i] == undefined || this[i] == '' || this[i] == null) {
 				return false
 			}
 		}
@@ -23,7 +23,7 @@ class Bd {
 	constructor() {
 		let id = localStorage.getItem('id')
 
-		if(id === null) {
+		if (id === null) {
 			localStorage.setItem('id', 0)
 		}
 	}
@@ -48,13 +48,13 @@ class Bd {
 		let id = localStorage.getItem('id')
 
 		// recuperar todas as despesas cadastradas em localStorage
-		for(let i = 1; i <= id; i++){
+		for (let i = 1; i <= id; i++) {
 
 			// Recupera as despesas
 			let expense = JSON.parse(localStorage.getItem(i))
 
 			// Verificando se existe a possibildiade de haver índices que foram pulados/ removidos. Nestes casos iremos pular esse índice
-			if(expense === null) {
+			if (expense === null) {
 				continue
 			}
 
@@ -78,16 +78,16 @@ function registerExpense() {
 	let value = document.getElementById('value')
 
 	let expense = new Expense(
-		year.value, 
-		month.value, 
-		day.value, 
-		type.value, 
+		year.value,
+		month.value,
+		day.value,
+		type.value,
 		description.value,
 		value.value
 	)
 
 
-	if(expense.validadeDate()) {
+	if (expense.validadeDate()) {
 		bd.save(expense)
 
 		document.getElementById('modal_title').innerHTML = 'Registro inserido com sucesso'
@@ -97,9 +97,9 @@ function registerExpense() {
 		document.getElementById('modal_btn').className = 'btn btn-success'
 
 		//dialog de sucesso
-		$('#modalRegisterExpense').modal('show') 
+		$('#modalRegisterExpense').modal('show')
 	} else {
-		
+
 		document.getElementById('modal_title').innerHTML = 'Erro na inclusão do registro'
 		document.getElementById('modal_title_div').className = 'modal-header text-danger'
 		document.getElementById('modal_context').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
@@ -107,7 +107,7 @@ function registerExpense() {
 		document.getElementById('modal_btn').className = 'btn btn-danger'
 
 		//dialog de erro
-		$('#modalRegisterExpense').modal('show') 
+		$('#modalRegisterExpense').modal('show')
 	}
 }
 
@@ -115,9 +115,52 @@ function loadListExpense() {
 
 	let expenses = Array()
 
-	expenses = bd.recoverAllRecords() 
+	expenses = bd.recoverAllRecords()
 
-	console.log(expenses)
-	
-	
+	// Selecionando o elemento tbody da tabela
+	let listExpenses = document.getElementById('listExpenses')
+
+	// Percorrer o array despensas, listando cada despesa de forma dinâmica
+	expenses.forEach(function(e) {
+		
+		// Criando a linha (tr)
+		let line = listExpenses.insertRow()
+
+		// Criando as colonuas (td)
+		 line.insertCell(0).innerHTML =  e.day + '/' + e.month + '/' + e.year
+		//  '${e.day}/${e.month}/${e.year}'
+		
+		// Ajustando o tipo
+		switch(e.type) {
+			case '1': e.type = 'Alimentação'
+				break
+			case '2': e.type = 'Educação'
+				break
+			case '3': e.type = 'Lazer'
+				break
+			case '4': e.type = 'Saúde'
+				break
+			case '5': e.type = 'Transporte'
+				break
+			case '6': e.type = 'Prestações'
+				break
+			case '7': e.type = 'Outros'
+				break
+		}
+		 line.insertCell(1).innerHTML = e.type
+		 line.insertCell(2).innerHTML = e.description
+		 line.insertCell(3).innerHTML = e.value
+	})
+
+	/*
+
+	 <tr>
+                <td>15/03/2018</td>
+                <td>Alimentação</td>
+                <td>Compras do mês</td>
+                <td>444.75</td>
+              </tr>
+	*/
+
+
 }
